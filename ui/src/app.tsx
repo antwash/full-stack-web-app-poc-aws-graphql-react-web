c0620edca9e4dfa81652c9cc922c44d526d818e4
Anthony Washington <awashington251@gmail.com>
2024-03-08 10:06:02 -0500

    use auto-gen graphql hooks

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,5 +1,35 @@
+import { useGetCarsQuery } from "./graphql/generated";
 export function App() {
-  return <div>Hey</div>;
+  const { data, loading } = useGetCarsQuery();
+
+  if (loading) {
+    return <h1>Loading....</h1>;
+  }
+
+  if (!data || data.cars.length === 0) {
+    return <h1>No car!</h1>;
+  }
+
+  return (
+    <table style={{ width: "20%" }}>
+      <tbody>
+        <tr>
+          <th>Model</th>
+          <th>Make</th>
+          <th>Year</th>
+        </tr>
+        {data.cars.map((car) => {
+          return (
+            <tr key={car.id}>
+              <td>{car.model}</td>
+              <td>{car.make}</td>
+              <td>{car.year}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
 export default App;
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,11 +3,20 @@ import * as ReactDOM from "react-dom/client";
 
 import App from "./app";
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql",
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>
 );
